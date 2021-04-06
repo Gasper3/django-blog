bc8430c8b5af35df357689274ff12961c0222a1d
Gasper3 <trzecik65@gmail.com>
2021-04-06 22:24:58 +0200

    Conditions to edit article

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import generic
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from .models import Article
+
+from pprint import pprint
 
 
 class IndexView(generic.ListView):
@@ -14,6 +18,14 @@ class ArticleShowView(generic.DetailView):
     template_name = 'blog/article/show.html'
 
 
+@method_decorator(login_required, name='get')
 class ArticleDetailView(generic.DetailView):
     model = Article
     template_name = 'blog/article/edit.html'
+
+    def get(self, request, *args, **kwargs):
+        article = Article.objects.get(slug=kwargs['slug'])
+        pprint(article)
+        if request.user != article.author:
+            return redirect('homepage', permanent=True)
+        return super().get(self, request)
