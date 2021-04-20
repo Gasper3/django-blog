c3cb68ec879fe96010fb442950f5070ee850c163
Gasper3 <trzecik65@gmail.com>
2021-04-20 22:56:11 +0200

    Changes in Article and Comment admin

--- blog/admin.py
+++ blog/admin.py
@@ -4,7 +4,15 @@ from .models import Article, Comment
 
 class ArticleAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status',)
+    list_display = ('title', 'status', 'author', 'created_at')
+    list_editable = ('status',)
+    search_fields = ('title', 'created_at',)
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('content', 'article', 'author', 'created_at',)
 
 
 admin.site.register(Article, ArticleAdmin)
-admin.site.register(Comment)
+admin.site.register(Comment, CommentAdmin)
