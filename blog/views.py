73c988dd31d4168d7536e883b29447f9bcd917ee
Gasper3 <trzecik65@gmail.com>
2021-04-26 22:50:26 +0200

    Deleted unused import

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect
 from django.views import generic
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
