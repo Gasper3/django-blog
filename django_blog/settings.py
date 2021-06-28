96d823a33077144971872926fd24b6f8b762af9b
Gasper3 <trzecik65@gmail.com>
2021-06-28 21:42:55 +0200

    Article content cache

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -72,6 +72,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'django_blog.wsgi.application'
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+        'LOCATION': '/var/tmp/django_cache',
+    }
+}
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
